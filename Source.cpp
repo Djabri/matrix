b296b138a061de0ec560d1fc4edbb450d7a3e782
Dzhabrail Elmezov <elmezov007@gmail.com>
2018-03-03 23:14:40 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -3,168 +3,247 @@
 #include <fstream>
 #include <string>
 
-#define FILE_PATH_1 "F:\\secondSem\\ÀßÏ\\labs\\matrix\\matrix\\A.txt"
-#define FILE_PATH_2 "F:\\secondSem\\ÀßÏ\\labs\\matrix\\matrix\\B.txt"
+#define FILE_PATH_1 "A.txt"
+#define FILE_PATH_2 "B.txt"
+#define INPUT_FILE "R.txt"
 
-class matrix_t {
+
+class matrix_t
+{
 private:
-	int ** data_;
-	unsigned int rows_;
-	unsigned int collumns_;
+	int **elements;
+	unsigned int rows;
+	unsigned int columns;
 public:
 	matrix_t();
-	matrix_t(std::string, std::string);
 	matrix_t(const matrix_t&);
+	~matrix_t();
+	
+	int get_rows() const;
+	int get_columns() const;
+
 
 	matrix_t add(matrix_t & other) const;
-	matrix_t sub(const matrix_t & other) const;
-	matrix_t mul(const matrix_t & other) const;
-	matrix_t T(const matrix_t & other) const;
+	matrix_t sub(matrix_t & other) const;
+	matrix_t mul(matrix_t & other) const;
+	matrix_t trans(matrix_t & other) const;
 
-
-	std::ifstream & read(std::ifstream &stream)
+	std::ifstream& read(std::ifstream & stream)
 	{
-		unsigned int rows;
-		unsigned int collumns;
-		char symbol;
+		char op;
+		int **elements_;
+		unsigned int rows_;
+		unsigned int columns_;
 
-		bool success = true;
-		if (stream >> rows && stream >> symbol && symbol == ',' && stream >> collumns) {
-			int ** data = new int *[rows];
-			for (std::size_t i = 0; success && i < rows; ++i) {
-				data[i] = new int[collumns];
-				for (std::size_t j = 0; j < collumns; ++j) {
-					if (!(stream >> data[i][j])) {
-						success = false;
-						break;
-					}
+		assert(stream.is_open());
+
+		if (stream >> rows_ && stream >> op && op == ',' && stream >> columns_)
+		{
+			elements_ = new int *[rows_];
+			for (unsigned int i = 0; i < rows_; i++)
+			{
+				elements_[i] = new int[rows_];
+			}		
+
+			for (unsigned int i = 0; i < rows_; i++)
+			{
+				for (unsigned int j = 0; j < columns_; j++)
+				{
+					stream >> elements_[i][j];
 				}
+				std::cout << "\n";
 			}
 
-			if (success) {
-				for (std::size_t i = 0; i < rows_; ++i) {
-					delete[] data_[i];
-				}
-				delete[] data_;
+			rows = rows_;
+			columns = columns_;
 
-				rows_ = rows;
-				collumns_ = collumns;
-				data_ = data;
+			elements = new int * [rows_];
+			for (unsigned int i = 0; i < rows; i++)
+			{
+				elements[i] = new int[columns_];
 			}
-			else {
-				for (std::size_t i = 0; i < rows; ++i) {
-					delete[] data[i];
+
+			for (unsigned int i = 0; i < rows_; i++)
+			{
+				for (unsigned int j = 0; j < columns_; j++)
+				{
+					elements[i][j] = elements_[i][j];
 				}
-				delete[] data;
+				std::cout << "\n";
 			}
+
+			for (unsigned int i = 0; i < rows_; i++)
+			{
+				delete[]elements_[i];
+			}
+
+			delete[]elements_;
 		}
-		else {
-			success = false;
-		}
-
-		if (!success) {
+		else
+		{
 			stream.setstate(std::ios_base::failbit);
 		}
 
+		stream.close();
 		return stream;
 	}
-
-	std::ostream & write(std::ostream & stream) const
+	std::ostream& write(std::ofstream & stream) const
 	{
-		stream << rows_ << ", " << collumns_;
-		for (std::size_t i = 0; i < rows_; ++i) {
-			stream << '\n';
-			for (std::size_t j = 0; j < collumns_; ++j) {
-				stream << data_[i][j];
-				if (j != rows_ - 1) {
-					stream << ' ';
-				}
+		for (unsigned int i = 0; i < rows; i++)
+		{
+			for (unsigned int j = 0; j < columns; j++)
+			{
+				stream << elements[i][j] << "  ";
 			}
+			stream << "\n";
 		}
-
+		stream.close();
 		return stream;
 	}
 };
 
-matrix_t::matrix_t() : rows(0), collumns(0), data(nullptr)
+int matrix_t::get_rows() const 
 {
+	return rows;
+}
+
+int matrix_t::get_columns() const 
+{
+	return columns;
+}
+
+matrix_t::matrix_t() : elements(nullptr), rows(0), columns(0)
+{
+
 }
 
 matrix_t::matrix_t(const matrix_t& other)
 {
-	rows_ = other.rows_;
-	collumns_ = other.collumns_;
+	rows = other.rows;
+	columns = other.columns;
 
-	data_ = new int *[rows_];
-	for (unsigned int i = 0; i < other.rows_; i++)
+	elements = new int *[rows];
+	for (unsigned int i = 0; i < rows; i++)
 	{
-		data_[i] = new int[other.collumns_];
-	}
-
-	for (unsigned int i = 0; i < other.rows_; i++)
-	{
-		for (unsigned int j = 0; j < other.collumns_; j++)
+		elements[i] = new int[columns];
+		for (unsigned int j = 0; j < columns; j++)
 		{
-			data_[i][j] = other.data_[i][j];
+			elements[i][j] = other.elements[i][j];
 		}
-	}
+	} 
 }
 
-//matrix_t::matrix_t(std::string stream, std::string stream_2)
-//{	
-//	char op;
-//	matrix_t A, B;
-//
-//	std::ifstream fileOne(stream);
-//	std::ifstream fileTwo(stream_2);
-//
-//	assert(fileOne.is_open() , fileTwo.is_open());
-//	
-//	if (fileOne >> A.rows && fileOne >> op && op == ',' && fileOne >> A.collumns)
-//	{
-//
-//	}
-//	else
-//	{
-//		fileOne.setstate(std::ios_base::failbit);
-//	}
-//
-//	for (unsigned int i = 0; i < A.rows; i++)
-//	{
-//		//for (unsigned int j = 0; j < B.rows; j++)
-//		//{
-//		//	fileOne >> A.data[i][j];
-//		//	fileTwo >> B.data[i][j];
-//		//}
-//		fileOne >> op;
-//		std::cout << std::endl;
-//	}
-//
-//
-//}
+matrix_t matrix_t::add(matrix_t & other) const
+{
+	matrix_t result;
+	
+	result.elements = new int *[rows];
 
-matrix_t matrix_t::add(matrix_t &other)const {
-
-	matrix_t re;
-
-	for (unsigned int i = 0; i < rows_; i++)
+	for (unsigned int i = 0; i < rows; i++)
 	{
-		for (unsigned int j = 0; j < collumns_; j++)
-		{
-			re.data_[i][j] = data_[i][j] + other.data_[i][j];
-		}
+		result.elements[i] = new int[columns];
 	}
 
-	return re;
+	if (other.rows == rows && other.columns == columns)
+	{
+		for (unsigned int i = 0; i < rows; i++) {
+			for (unsigned int j = 0; j < columns; j++) {
+				result.elements[i][j] = other.elements[i][j] + elements[i][j];
+			}
+		}
+
+		result.rows = other.rows;
+		result.columns = other.columns;
+	}
+	else
+	{
+
+	}
+
+	return result;
 }
 
+matrix_t matrix_t::sub(matrix_t & other) const
+{
+
+}
+
+matrix_t matrix_t::mul(matrix_t & other) const
+{
+
+
+}
+
+matrix_t matrix_t::trans(matrix_t & other) const
+{
+
+}
+
+matrix_t::~matrix_t()
+{
+	for (unsigned int i = 0; i < rows; i++)
+	{
+		delete[]elements[i];
+	}
+
+	delete[]elements;
+}
 
 int main()
 {
-	std::string fileOne = FILE_PATH_1;
-	std::string fileTwo = FILE_PATH_2;
+	std::ifstream fileOne(FILE_PATH_1);
+	std::ifstream fileTwo(FILE_PATH_2);
+	std::ofstream input_file(INPUT_FILE);
 
-	matrix_t A(fileOne, fileTwo);
+
+	matrix_t A;
+	matrix_t B;
+
+	A.read(fileOne);
+	B.read(fileTwo);
+
+	char check;
+
+	std::cout << "Select an arithmetic operation '+', '-', '*', 'T' \n";
+
+	if (A.read(fileOne) && B.read(fileTwo))
+	{
+		switch (check)
+		{
+		case '+':
+		{
+			A.add(B).write(input_file);
+			break;
+		}
+		case '-':
+		{
+			A.sub(B).write(input_file);
+			break;
+		}
+
+		case '*':
+		{
+			A.mul(B).write(input_file);
+			break;
+		}
+
+		case 'T':
+		{
+			A.trans.write(input_file);
+			break;
+		}
+
+		default:
+			std::cout << "An error has occured while reading input data\n";
+			break;
+		}
+	}
+	else
+	{
+		std::cout << "Select an arithmetic operation '+', '-', '*', '/'";
+	}
+
 	std::cin.get();
 	return 0;
 }
+
